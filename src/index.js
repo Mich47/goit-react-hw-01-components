2051fdb2ddacea21668a50b6cd06b70211e94a87
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-12-19 23:50:28 +0200

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App';
 import { theme } from './theme';
-
-import { Profile } from 'components/Profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import user from './assets/user.json';
-import data from './assets/data.json';
-import friends from './assets/friends.json';
-import transactions from './assets/transactions.json';
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
